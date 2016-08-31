9b92a4b09ce3161cbf9f84e9d25b4c6d0276986b
kdonovan2005 <kdonovan2005@gmail.com>
2016-08-31 14:22:29 -0400

    added basic routing and started CRUD

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+
+  def index
+    @comments = Comment.all
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Post.create(title: "First Test", body: "Body content of the first test post.")
+
+Comment.create(content: "First test comment", name: "Kristin", email: "kd@me.com", post_id: 1)
